926bb14af63e890fbf969eaf296ae54102e7ed83
Justin Taylor <justin2taylor@gmail.com>
2021-11-04 18:53:11 -0700

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# After Effects Command Ids
+
+_Find and use Command IDs for After Effects across versions_
+
+---
+
+## Viewer
+
+Open the viewer to quickly search and find the Command ID for your version of After Effects:
+
+## Usage
+
+Use Command IDs in After Effects with `app.executeCommand();`
+
+## JSON
+
+Download the complete list of JSON file for each version of After Effects in the `json` folder.
+
+---
+
+## Develop Quickstart
+
+- Install: `yarn`
+- Develop: `yarn dev`
+- Build: `yarn build`
---
+++ src/app.tsx
@@ -0,0 +1,114 @@
+import * as React from "react";
+const { useState } = React;
+
+import cmds2022 from "../json/2022.json";
+import cmds2021 from "../json/2021.json";
+import cmds2020 from "../json/2020.json";
+import cmds2019 from "../json/2019.json";
+import cmds2018 from "../json/2018.json";
+import cmds2017 from "../json/2017.json";
+import cmds2015_3 from "../json/2015.3.json";
+import cmds2015 from "../json/2015.json";
+
+import "./styles.css";
+
+interface DataByYear {
+  [key: string]: string;
+}
+interface DataCommand {
+  [key: string]: string;
+}
+interface DataByCommands {
+  [key: string]: DataCommand;
+}
+interface FormatPair {
+  obj: DataByYear;
+  year: string;
+}
+
+const years = [
+  "2022",
+  "2021",
+  "2020",
+  "2019",
+  "2018",
+  "2017",
+  "2015_3",
+  "2015",
+];
+const cmdList: DataByCommands = {};
+
+const formatData = (data: FormatPair[]) => {
+  data.map(({ obj, year }) => {
+    console.log(obj, year);
+    Object.keys(obj).map((key) => {
+      const cmd = obj[key] as keyof DataCommand;
+      if (!cmdList[cmd]) {
+        cmdList[cmd] = { [year]: key };
+      } else {
+        cmdList[cmd][year] = key;
+      }
+    });
+  });
+};
+
+formatData([
+  { obj: cmds2022, year: "2022" },
+  { obj: cmds2021, year: "2021" },
+  { obj: cmds2020, year: "2020" },
+  { obj: cmds2019, year: "2019" },
+  { obj: cmds2018, year: "2018" },
+  { obj: cmds2017, year: "2017" },
+  { obj: cmds2015_3, year: "2015_3" },
+  { obj: cmds2015, year: "2015" },
+]);
+
+export default () => {
+  const [val, setVal] = useState("");
+  return (
+    <div className="main">
+      <h2>After Effects Command IDs [2015 - 2022]</h2>
+      <input
+        className="searchbox"
+        type="text"
+        onChange={(e) => setVal(e.target.value)}
+        value={val}
+        spellCheck={false}
+        placeholder="search..."
+      />
+      <table>
+        <tbody>
+          <tr className="result result-key result-header" key={-1}>
+            <th key={0} className="result-name">
+              Name
+            </th>
+            {years.map((year, i) => (
+              <th key={i + 1} className="result-year">
+                {year.replace("_", ".")}
+              </th>
+            ))}
+          </tr>
+          {Object.keys(cmdList)
+            .sort()
+            .map(
+              (cmd, i) =>
+                cmd.toLowerCase().indexOf(val.toLowerCase()) > -1 && (
+                  <tr className="result" key={i}>
+                    <td className="result-name">{cmd}</td>
+                    {years.map((year) => (
+                      <td
+                        className={`result-year ${
+                          !cmdList[cmd][year] && "result-na"
+                        }`}
+                      >
+                        {cmdList[cmd][year] || "N/A"}
+                      </td>
+                    ))}
+                  </tr>
+                )
+            )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
